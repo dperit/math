7c1eb15adc006cafd2187b2bada54256bbc98111
Alan Kligman <alan.kligman@gmail.com>
2012-02-17 18:30:16 -0500

    Added rotation to matrix3.

--- src/matrix/matrix3.js
+++ src/matrix/matrix3.js
@@ -112,6 +112,54 @@ define( function ( require ) {
                 }
                 return result;
             },
+            
+            // Convert a vector rotation (in radians) to a 3x3 matrix
+            rotate: function( v, result ) {
+                var r = result || matrix4.identity;
+
+                var sinA,
+                    cosA;
+
+                if( 0 !== v[2] ) {
+                    sinA = Math.sin( v[2] );
+                    cosA = Math.cos( v[2] );
+                    ml = [];
+                    ml.push(matrix3.$([ cosA, sinA, 0,
+                                       -sinA, cosA, 0,
+                                        0, 0, 1 ] ));
+                    ml.push(matrix3.$(r));
+                    
+                    matrix3.multiply( ml, r );
+                }
+
+                if( 0 !== v[1] ) {
+                    sinA = Math.sin( v[1] );
+                    cosA = Math.cos( v[1] );
+                    ml = [];
+                    ml.push(matrix3.$([ cosA, 0, -sinA,
+                                        0, 1, 0,
+                                        sinA, 0, cosA ] ));
+                    ml.push(matrix3.$(r));
+                    
+                    matrix3.multiply( ml, r );
+                }
+
+                if( 0 !== v[0] ) {
+                    sinA = Math.sin( v[0] );
+                    cosA = Math.cos( v[0] );
+                    ml = [];
+                    ml.push(matrix3.$([ 1, 0, 0,
+                                        0, cosA, sinA,
+                                        0, -sinA, cosA ] ));
+                    ml.push(matrix3.$(r));
+                    
+                    matrix3.multiply( ml, r );
+                }
+
+                if( !result ) {
+                    return r;
+                }
+            },
 
             transpose: function( m, result ) {
                 result = result || Matrix3();
