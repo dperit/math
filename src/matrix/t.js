92e91f3b501aef3a15ccd24d5f483139fa6b4aa5
David Perit <dperit@gmail.com>
2012-07-20 17:01:46 -0400

    Fixed bug in t Began updating transform-api.test (it's still mostly broken, though. The expected results need to be updated for a lot of things because translate, rotate, and scale work differently now) Included T in the combined math file

--- src/matrix/t.js
+++ src/matrix/t.js
@@ -3,8 +3,8 @@ define( function ( require ) {
   return function( FLOAT_ARRAY_TYPE ) {
 
     var M = require( "matrix/m" );
-    var M4 = require( "matrix/m4" );
-    var transform = require("matrix/transform-api");
+    var M4 = require( "matrix/m4" )( FLOAT_ARRAY_TYPE );
+    var transform = require("matrix/transform-api")( FLOAT_ARRAY_TYPE );
 
     var T = function(t, r, s) {
       var matrix = new M4();
--- src/matrix/transform-api.test.js
+++ src/matrix/transform-api.test.js
@@ -17,9 +17,9 @@ define(
 
         var rotation = [this.math.TAU/2, this.math.TAU/3, this.math.TAU/4];
         var transform = this.math.transform.rotate( rotation );
-        var expected = [0, -1/2, -Math.sqrt(3)/2, 0,
-                        1, 0, 0, 0,
-                        0, -Math.sqrt(3)/2, 1/2, 0,
+        var expected = [0, -1, 0, 0,
+                        1/2, 0, -Math.sqrt(3)/2, 0,
+                        Math.sqrt(3)/2, 0, 1/2, 0,
                         0, 0, 0, 1];
         ok( this.math.matrix4.equal( transform, expected ), "transform is correct" );
       });
@@ -30,9 +30,9 @@ define(
         var rotation = [this.math.TAU/2, this.math.TAU/3, this.math.TAU/4];
         var transform = this.math.M4( this.math.matrix4.identity );
         this.math.transform.rotate( rotation, transform );
-        var expected = [0, -1/2, -Math.sqrt(3)/2, 0,
-                        1, 0, 0, 0,
-                        0, -Math.sqrt(3)/2, 1/2, 0,
+        var expected = [0, -1, 0, 0,
+                        1/2, 0, -Math.sqrt(3)/2, 0,
+                        Math.sqrt(3)/2, 0, 1/2, 0,
                         0, 0, 0, 1];
         ok( this.math.matrix4.equal( transform, expected ), "transform is correct" );
       });
